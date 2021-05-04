4a4b15d6a9a3a8c2bb85ab5e7d7ed25387807aad
roman89ua <roman89ua@gmail.com>
2021-05-04 22:25:00 +0300

    Set upped the project

--- README.md
+++ README.md
@@ -68,3 +68,6 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `yarn build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+dependencies after initialization of the project:
+yarn add @babel/plugin-proposal-decorators mobx mobx-react mobx-react-devtools --dev
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
+// import DevTools from "mobx-react-devtools";
+
+class App extends Component{
+    render () {
+        return (
+            <div className="App">
+                {/*<DevTools />*/}
+                <h1>Hello MobX with react</h1>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister();
